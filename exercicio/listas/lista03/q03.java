e5d41f4ea0626c00762262218a5dfce7b2489a39
laurindolucas <caiolucax2024@gmail.com>
2025-10-29 14:27:31 -0300

    adicionando q03 da lista 03

--- exercicio/listas/lista03/q03.java
+++ exercicio/listas/lista03/q03.java
@@ -1,18 +1,54 @@
 package lista03;
-import java.Util.Scanner;
+import java.util.Scanner;
 public class q03 {
 
-    public static int Busca(){
-
-    }
-
+    public static int[] busca(int [] v, int obj){
+        int ini, meio, fim;
+        ini = 0;
+        fim = v.length - 1;
+        int passos = 0;
+        while( ini <= fim){
+            passos++;
+            meio = (ini + fim)/2;
+            if (v[meio] == obj) {
+                return new int[]{meio, passos};
+            }
+            else if (v[meio] > obj) {
+                fim = meio - 1;
+            }else if (v[meio] < obj){
+                ini = meio + 1;
+            }
+        }
+        return new int[]{-1, passos};
+        }
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite o tamanho do vetor");
-        int [] v = scanner.nextInt();
+        int tamV = scanner.nextInt();
 
-        for(int i = 0; i<)
+        int [] v = new int[tamV];
+        System.out.println("Digte os elementos do vetor");
+        for(int i = 0; i < tamV; i++){
+            int valor = scanner.nextInt();
+            if (i > 0 && valor < v[i - 1]) {
+                System.out.println("Erro: o vetor deve ser digitado em ordem crescente!");
+                i--;
+                continue;
+            }
+            v[i] = valor;
+        }
+        System.out.println("Digte o número que deseja buscar");
+        int obj = scanner.nextInt();
+
+        int [] resultado = busca(v, obj);
+
+        if(resultado[0]== -1){
+           System.out.println("-1");
+        }else{
+            System.out.print(resultado[0] + " ");
+            System.out.print(resultado[1]);
+        }
     }
 }
