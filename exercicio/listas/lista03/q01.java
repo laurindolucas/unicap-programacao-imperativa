b2567c67183151c56f0ae2d89e559b1dfd276a3a
laurindolucas <caiolucax2024@gmail.com>
2025-10-28 22:38:49 -0300

    adicionando q01 da lista 03

--- exercicio/listas/lista03/q01.java
+++ exercicio/listas/lista03/q01.java
@@ -1,4 +1,29 @@
 package lista03;
+import java.util.Scanner;
 
 public class q01 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o número que deseja buscar: ");
+        int busca = scanner.nextInt();
+        System.out.println("Digite o tamanho do vetor: ");
+        int tamVetor = scanner.nextInt();
+
+        int [] v = new int[tamVetor];
+        System.out.println("Digite os valores do vetor: ");
+        for(int i = 0; i < tamVetor; i++ ){
+            v[i] = scanner.nextInt();
+        }
+        boolean encontrado = false;
+
+        for(int j = 0; j < tamVetor; j++ ){
+            if(v[j] == busca){
+                System.out.println(j);
+            }
+        }
+
+        if(!encontrado){
+            System.out.println(-1);
+        }
+    }
 }
--- exercicio/listas/lista03/q03.java
+++ exercicio/listas/lista03/q03.java
@@ -1,12 +1,18 @@
 package lista03;
 import java.Util.Scanner;
 public class q03 {
+
+    public static int Busca(){
+
+    }
+
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        int [] v = new int [4, 9, 10, 13, 15, 16, 18, 20];
+        System.out.println("Digite o tamanho do vetor");
+        int [] v = scanner.nextInt();
 
-
-
+        for(int i = 0; i<)
     }
 }
